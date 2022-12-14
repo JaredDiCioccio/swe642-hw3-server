24876d44cad73da700a8d0a411198717dc97fad0
Jared DiCioccio <jared.dicioccio@gmail.com>
2022-12-14 13:26:58 -0500

    Removed data source exclusion

--- src/main/java/com/gmu/jmd/Homework3ServerApplication.java
+++ src/main/java/com/gmu/jmd/Homework3ServerApplication.java
@@ -30,7 +30,7 @@ import java.util.List;
 import java.util.Scanner;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 @EnableWebMvc
